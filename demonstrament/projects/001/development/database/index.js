85e64ca7e18490e3e099eeed7e963f08969321fb
Thomas Patrick Welborn <thomas.patrick.welborn@outlook.com>
2024-06-09 09:10:09 -0700

    Demonstrament 001

--- demonstrament/projects/001/development/database/index.js
+++ demonstrament/projects/001/development/database/index.js
@@ -1,8 +1,3 @@
 import {
-  Model as Model,
-  Schema as Schema,
-  createConnection as createConnection,
-} from "mongoose"
-import {
   express as express,
 } from "express"
---
+++ demonstrament/projects/001/development/database/schemata/cycles/spreadsheet-to-filesystem/spreadsheet-to-spreadsheet-database/index.js
@@ -0,0 +1,4 @@
+import {
+  Schema
+} from "mongoose"
+const SpreadsheetToSpreadsheetDatabase = new Schema()
