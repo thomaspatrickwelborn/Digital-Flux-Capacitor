d1e38572f2192d1cb73f191be0bbd175336651d2
Thomas Patrick Welborn <thomas.patrick.welborn@outlook.com>
2024-06-19 14:01:42 -0700

    VIEW Spreadsheet Class

--- development/cycles/spreadsheet-to-filesystem/filesystem-database-to-filesystem/Templates/index.js
+++ development/cycles/spreadsheet-to-filesystem/filesystem-database-to-filesystem/Templates/index.js
@@ -1,12 +1,12 @@
-import * as coutil from './coutil/index.js'
-import * as es_markup from './es_markup/index.js'
+import * as Coutil from './Coutil/index.js'
+import * as ES_Markup from './ES_Markup/index.js'
 // import es_module from './es_module/index.js'
 // import json_file from './json_file/index.js'
 // import sass_style from './sass_style/index.js'
 
 export {
-  coutil,
-  es_markup,
+  Coutil,
+  ES_Markup,
   // es_module,
   // json_file,
   // sass_style,
