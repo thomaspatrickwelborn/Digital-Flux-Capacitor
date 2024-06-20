2b804d001db2652ff7bb679885eb3a2a83568925
Thomas Patrick Welborn <thomas.patrick.welborn@outlook.com>
2024-06-19 17:09:21 -0700

    VIEW Spreadsheet Class

--- development/cycles/spreadsheet-to-filesystem/filesystem-database-to-filesystem/Templates/ES_Markup/Blocks/Block/Element/index.js
+++ development/cycles/spreadsheet-to-filesystem/filesystem-database-to-filesystem/Templates/ES_Markup/Blocks/Block/Element/index.js
@@ -1,56 +1,65 @@
+import Blocks from '../../index.js'
 export default function Element($data) {
   const { coutils, content } = $data
   const { blocks, element } = content
   const { operators, parseTen } = coutils
   const _element = []
-  // if(element === undefined) return
-  // const {
-  //   tag, attribute, text, data
-  // } = element
-  // if(tag === undefined) return
-  // var { pos } = tag
-  // pos = pos || {}
-  // const inpos = pos.in
-  // const expos = pos.ex
-  // -%>
-  // <%- inpos %><%- parseTen(tag.name) %>
-  // <%
-  // iterateAttributes:
-  // for(const $attribute of attribute) {
-  // -%>
-  //   <%
-  //   if(
-  //     $attribute.key !== undefined &&
-  //     $attribute.ten !== undefined &&
-  //     $attribute.ten !== operators.tenSlug
-  //   ) { -%>
-  //     <%- $attribute.key %>="<%- $attribute.ten %>"
-  //   <% } else
-  //   if(
-  //     $attribute.key !== undefined &&
-  //     $attribute.ten === undefined
-  //   ) { -%>
-  //     <%- $attribute.key %>
-  //   <% } else
-  //   if(
-  //     $attribute.key === undefined &&
-  //     $attribute.ten !== undefined &&
-  //     $attribute.ten !== operators.tenSlug
-  //   ) { -%>
-  //     <%- $attribute.ten %>
-  //   <% } -%>
-  // <% } -%>
-  // <%- expos %>
-  // <% if(blocks.length) { -%>
-  //   <%- await include('../../index.ejs', {
-  //     content: blocks,
-  //     coutils: coutils,
-  //   }) %>
-  // <% } -%>
-  // <% if(
-  //   !operators.void.includes(tag.name)
-  // ) { -%>
-  //   <%- inpos %>/<%- parseTen(tag.name) %><%- expos %>
-  // <% } -%>
+  if(element === undefined) return _element
+  const {
+    tag, attributes, text, data
+  } = element
+  if(tag === undefined) return _element
+  var { pos } = tag
+  pos = pos || {}
+  const inpos = pos.in
+  const expos = pos.ex
+  _element.push(
+    [inpos, parseTen(tag.name)]
+  )
+  iterateAttributes:
+  for(const $attribute of attributes) {
+    if(
+      $attribute.key !== undefined &&
+      $attribute.ten !== undefined &&
+      $attribute.ten !== operators.tenSlug
+    ) { 
+      _element.push(
+        [$attribute.key, '=', '"$attribute.ten"']
+      )
+     } else
+    if(
+      $attribute.key !== undefined &&
+      $attribute.ten === undefined
+    ) { 
+      _element.push(
+        [$attribute.key]
+      )
+     } else
+    if(
+      $attribute.key === undefined &&
+      $attribute.ten !== undefined &&
+      $attribute.ten !== operators.tenSlug
+    ) { 
+      _element.push(
+        [$attribute.ten]
+      )
+     } 
+   } 
+   _element.push(expos)
+  if(blocks.length) { 
+    _element.push(
+      Blocks({
+        content: blocks,
+        coutils: coutils,
+      })
+    )
+  } 
+   if(
+    !operators.void.includes(tag.name)
+  ) { 
+    _element.push(
+      [inpos, '/', parseTen(tag.name),  expos]
+    )
+   } 
   return _element
 }
--- development/cycles/spreadsheet-to-filesystem/filesystem-database-to-filesystem/Templates/ES_Markup/Blocks/Block/Statement/index.js
+++ development/cycles/spreadsheet-to-filesystem/filesystem-database-to-filesystem/Templates/ES_Markup/Blocks/Block/Statement/index.js
@@ -1,31 +1,37 @@
+import Blocks from '../../index.js'
 export default function Statement($data) {
   const { coutils, content } = $data
   const { operators, parseTen } = coutils
   const { blocks, statement } = content
   const _statement = []
-  // if(statement === undefined) return
-  // const { lexter, dexter } = statement
-  // const expressions = [lexter, dexter]
-  // var expressionsIndex = 0
-  // iterateExpressions:
-  // while(expressionsIndex < expressions.length) {
-  //   const expression = expressions[expressionsIndex]
-  //   if(expression === undefined) {
-  //     expressionsIndex++
-  //     continue iterateExpressions
-  //   }
-  //   var { ser, ten, per, pos, par } = expression
-  //   pos = pos || {}
-  //   const inpos = pos.in
-  //   const expos = pos.ex
-  //   -%>
-  //   <%- ser %> <%- parseTen(ten) %> <%- per %> <%- inpos %>
-  //   <%- await include('../../index.ejs', {
-  //       content: blocks,
-  //       coutils: coutils,
-  //   }) %>
-  //   <%- expos %><%- par %>
-  //   <% expressionsIndex++ -%>
-  // <% } -%>
+  if(statement === undefined) return
+  const { lexter, dexter } = statement
+  const expressions = [lexter, dexter]
+  var expressionsIndex = 0
+  iterateExpressions:
+  while(expressionsIndex < expressions.length) {
+    const expression = expressions[expressionsIndex]
+    if(expression === undefined) {
+      expressionsIndex++
+      continue iterateExpressions
+    }
+    var { ser, ten, per, pos, par } = expression
+    pos = pos || {}
+    const inpos = pos.in
+    const expos = pos.ex
+    _statement.push(
+      [ser, parseTen(ten), per, inpos] 
+    )
+    _statement.push(
+      Blocks({
+        content: blocks,
+        coutils: coutils,
+      })
+    ) 
+    _statement.push(
+      [expos, par]
+    )
+    expressionsIndex++
+  }
   return _statement
 }
