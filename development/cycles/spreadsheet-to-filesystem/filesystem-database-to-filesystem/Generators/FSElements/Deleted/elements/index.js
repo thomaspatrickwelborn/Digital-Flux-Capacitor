f5899986ec23167c3e6bacf0f0aeff573e18465b
Thomas Patrick Welborn <thomas.patrick.welborn@outlook.com>
2024-07-01 18:15:34 -0700

    Filesystem Elements

--- development/cycles/spreadsheet-to-filesystem/filesystem-database-to-filesystem/Generators/FSElements/Deleted/elements/index.js
+++ development/cycles/spreadsheet-to-filesystem/filesystem-database-to-filesystem/Generators/FSElements/Deleted/elements/index.js
@@ -1,36 +1,46 @@
 import path from 'node:path'
 import { rm, rmdir, open, opendir, mkdir, stat } from 'node:fs/promises'
 export default async function DeletedElements($collection, $fsRootPath, $deleted) {
-  console.log('$deleted', $deleted)
+  const collectionLength = $collection.length
   const deleted = []
   const deletedFSElements = $deleted
   const deletedFSElementsLength = deletedFSElements.length
-  var deletedFSElementsIndex = 0
-  while(deletedFSElementsIndex < deletedFSElementsLength) {
+  let deletedFSElementsIndex = deletedFSElementsLength - 1
+  iterateDeletedFSElements: 
+  while(deletedFSElementsIndex > -1) {
     const deletedFSElement = deletedFSElements[deletedFSElementsIndex]
     .replace($fsRootPath.concat('/'), '')
-    // console.log('deletedFSElementDoc', deletedFSElementDoc)
-    // if(deletedFSElementDoc.fs.operations.delete === false) {
-    //   deletedFSElementsIndex++
-    //   continue
-    // }
-    // const deletedFSElementDocPath = path.join(
-    //   $fsRootPath, deletedFSElementDoc.fs.path
-    // )
-    // const deletedFSElementStat = await stat(deletedFSElement)
-    // if(deletedFSElementStat?.isFile() === true) {
-    //   rm(deletedFSElement, {
-    //     force: false, recursive: true
-    //   })
-    //   deleted.push(deletedFSElement)
-    // } else 
-    // if(deletedFSElementStat?.isFile() === false) {
-    //   rmdir(deletedFSElement, {
-    //     force: false, recursive: true
-    //   })
-    //   deleted.push(deletedFSElement)
-    // }
-    deletedFSElementsIndex++
+    let collectionIndex = 0
+    iterateCollectionDocs: 
+    while(collectionIndex < collectionLength) {
+      const collectionDoc = $collection[collectionIndex]
+      const collectionDocPath = collectionDoc?.fs?.path
+      const collectionDocOperationDelete = collectionDoc.fs.operations.delete
+      const deletedFSElementMatch = deletedFSElement.match(
+        new RegExp(`^${collectionDocPath}`)
+      )
+      if(
+        deletedFSElementMatch &&
+        collectionDocOperationDelete
+      ) {
+        const deletedFSElementDocPath = path.join(
+          $fsRootPath, deletedFSElement
+        )
+        const deletedFSElementStat = await stat(deletedFSElementDocPath)
+        if(
+          deletedFSElementStat?.isFile() === true ||
+          deletedFSElementStat?.isFile() === false
+        ) {
+          rm(deletedFSElementDocPath, {
+            force: false, recursive: true
+          })
+          deleted.push(deletedFSElement)
+          break iterateCollectionDocs
+        }
+      }
+      collectionIndex++
+    }
+    deletedFSElementsIndex--
   }
   return deleted
 }
