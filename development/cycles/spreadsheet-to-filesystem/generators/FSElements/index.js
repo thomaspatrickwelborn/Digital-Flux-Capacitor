7035b4d8ecd5cb39d188c6327115da36440787eb
Thomas Patrick Welborn <thomas.patrick.welborn@outlook.com>
2024-07-28 17:03:00 -0700

    Generators
    Mistarget

--- development/cycles/spreadsheet-to-filesystem/generators/FSElements/index.js
+++ development/cycles/spreadsheet-to-filesystem/generators/FSElements/index.js
@@ -1,6 +1,6 @@
 import EventEmitter from 'node:events'
 import path from 'node:path'
-import { mkdir, stat } from 'node:fs'
+import { writeFile, mkdir, stat } from 'node:fs'
 import { globSync } from 'glob'
 import chokidar from 'chokidar'
 
@@ -72,100 +72,59 @@ export default class FSElements extends EventEmitter {
   async #fsRootWatchChange($workbookPath) {
     this.fsRoot = this.fsRootPath
   }
-  add() {
-    let addedFSElementStat = stat(addedFSElementDocPath, (
+  addFile($addedFileDoc) {
+    let addedFSElementStat = stat($addedFileDoc.fs.path, (
       $err, $addedFSElementStat
     ) => {
+      console.log($err, $addedFSElementStat)
       if(
         $err || 
         $addedFSElementStat.isFile() === false &&
         $addedFSElementStat.isDirectory() === false
       ) {
-        switch(addedFSElementDoc.fs.type) {
-          case 'File':
-            writeFile(addedFSElementDoc, '', ($err) => {
-              if($err) return
-              this.emit('added:file', addedFSElementDoc)
-            })
-            break
-          case 'Fold':
-            mkdir(addedFSElementDocPath, {
-              recursive: true,
-            }, ($err, $dir) => {
-              if($err) return
-              this.emit('added:fold', addedFSElementDoc)
-            })
-            break
-        }
+        writeFile($addedFileDoc.fs.path, '', ($err) => {
+          console.log($err)
+          if($err) return
+          // this.emit('addFile', $addedFileDoc)
+        })
+      }
+    })
+  }
+  addFold($addedFoldDoc) {
+    let addedFSElementStat = stat($addedFoldDoc.fs.path, (
+      $err, $addedFSElementStat
+    ) => {
+      console.log($err, $addedFSElementStat)
+      if(
+        $err || 
+        $addedFSElementStat.isFile() === false &&
+        $addedFSElementStat.isDirectory() === false
+      ) {
+        mkdir($addedFoldDoc.fs.path, {
+          recursive: true,
+        }, ($err, $dir) => {
+          console.log($err)
+          if($err) return
+          // this.emit('addFold', $addedFoldDoc)
+        })
       }
     })
   }
   inputFileDoc($fileDoc) {
     const fileDoc = $fileDoc.toObject()
     const { operations, permissions, path } = fileDoc.fs
-    // console.log('path', path)
-    // console.log('fsRoot', this.fsRoot)
-    console.log('operations', operations)
     if(
       operations.add === true &&
       this.fsRoot.includes(path) === false
     ) {
-      console.log('fileDoc', fileDoc)
+      switch(fileDoc.fs.type) {
+        case 'File':
+          this.addFile(fileDoc)
+          break
+        case 'Fold':
+          this.addFile(fileDoc)
+          break
+      }
     }
-    // if(this.fsRoot.includes($fil))
-    
   }
-  // get added() { return this.#_added }
-  // set added($added) {
-  //   // 
-  // }
-  // get updated() { return this.#_updated }
-  // set updated($updated) {
-  //   // 
-  // }
-  // get deleted() { return this.#_deleted }
-  // set deleted($deleted) {
-  //   // 
-  // }
-
-  // input($collect) {
-    // const collectDocsLength = $collect.length
-    // let collectDocsIndex = 0
-    // while(collectDocsIndex < collectDocsLength) {
-    //   const collectDoc = $collect[collectDocsIndex]
-    //   console.log('collectDoc', collectDoc)
-    //   collectDocsIndex++
-    // }
-    // for(const $collectDoc of Object.values($collect)) {
-    //   console.log('$collectDoc', $collectDoc)
-    // }
-    // const fsVine = $collection.reduce(
-    //   ($fsVine, $collectionDoc) => {
-    //     if($collectionDoc.fs.path === undefined) return $fsVine
-    //     $fsVine.push(
-    //       path.join(fsRootPath, $collectionDoc.fs.path)
-    //     )
-    //     return $fsVine
-    //   }, []
-    // )
-    // const added = new Added({
-    //   collection: $collection, 
-    //   fs: {
-    //     rootPath: fsRootPath,
-    //     root: fsRoot,
-    //     vine: fsVine
-    //   }
-    // })
-    // added.on('added:fold', ($addedFold) => {
-    //   console.log('added:fold', $addedFold)
-    // })
-    // const updated = await Updated(
-    //   $collection, fsRootPath, fsRoot, fsVine
-    // )
-    // const deleted = await Deleted(
-    //   $collection, fsRootPath, fsRoot, fsVine
-    // )
-    // return $collection
-    //   return this
-  // }
 }
